cc0e3c1dc4328a00531225e1f4f743ef3f9d6edb
shaunelam <58092676+shaunelam@users.noreply.github.com>
2019-12-20 15:59:46 -0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "shaun-elam-notes-apps-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://jpcy686n7k.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_Y5YdBD3DD",
+      APP_CLIENT_ID: "3bv20md9n8mm9o0haieo9phule",
+      IDENTITY_POOL_ID: "us-east-2:8682f1b3-35fe-4e9e-b4d8-8ee308798249"
+    }
+  };
