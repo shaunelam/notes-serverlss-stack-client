7296ad25f1b169c20451661850fea0eaca3d0aae
shaunelam <58092676+shaunelam@users.noreply.github.com>
2019-12-25 10:58:32 -0800

    More Updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
 import { Navbar, NavItem, Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
+import { Auth } from "aws-amplify";
 
 function App(props) {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+  
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+  
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+    userHasAuthenticated(false);
+    props.history.push("/login");
+  }
+
   return (
+    !isAuthenticating &&
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
@@ -17,18 +46,29 @@ function App(props) {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem>Signup</NavItem>
+            {isAuthenticated
+              ?
+             <>
+            <LinkContainer to="/settings">
+              <NavItem>Settings</NavItem>
             </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem>Login</NavItem>
-            </LinkContainer>
+            <NavItem onClick={handleLogout}>Logout</NavItem>
+             </> 
+              : <>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </>
+            }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
     </div>
   );
 }
 
-export default App;
+export default withRouter (App);
--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,56 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/login";
+import AppliedRoute from "./components/AppliedRoute";
+import ResetPassword from "./containers/ResetPassword";
+import Signup from "./containers/Signup";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import Settings from "./containers/Settings";
+import ChangePassword from "./containers/ChangePassword";
+import ChangeEmail from "./containers/ChangeEmail";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
-export default function Routes() {
+export default function Routes({ appProps }) {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+      <AppliedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
+      <AppliedRoute
+        path="/signup"
+        exact
+        component={Signup}
+        appProps={appProps}
+      />
+      <AuthenticatedRoute
+        path="/settings/password"
+        exact
+        component={ChangePassword}
+        appProps={appProps}
+      />
+      <AuthenticatedRoute
+        path="/settings/email"
+        exact
+        component={ChangeEmail}
+        appProps={appProps}
+      />
+      <AuthenticatedRoute
+        path="/Settings"
+        exact
+        component={Settings}
+        appProps={appProps}
+      />
+      <UnauthenticatedRoute
+        path="/login/reset"
+        exact
+        component={ResetPassword}
+        appProps={appProps}
+      />
+      {/* Finally, catch all unmatched routes */}
       <Route component={NotFound} />
     </Switch>
   );
