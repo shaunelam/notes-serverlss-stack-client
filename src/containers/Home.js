6765f4a8f64fb636d63561cf78e4c1bc8601ca2b
shaunelam <58092676+shaunelam@users.noreply.github.com>
2020-02-05 12:39:42 -0800

    Project complete

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Home.css";
 import { API } from "aws-amplify";
-import { LinkContainer } from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from "react-router-dom";
 
 export default function Home(props) {
   const [notes, setNotes] = useState([]);
@@ -56,6 +57,10 @@ export default function Home(props) {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">Login</Link>
+          <Link to="/signup" className="btn btn-success btn-lg">Signup</Link>
+        </div>
       </div>
     );
   }
